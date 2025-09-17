e6135b03881607e53b771fc49483e42c64b42aec
c0d3r_ <147667582+coderthegreat2@users.noreply.github.com>
2025-09-16 22:42:43 -0400

    mod setup

---
+++ src/main/java/com/c0d3r_/thirdlife/ThirdLife.java
@@ -0,0 +1,18 @@
+package com.c0d3r_.thirdlife;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ThirdLife implements ModInitializer {
+	public static final String MOD_ID = "thirdlife";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+
+        LOGGER.info("Hello Fabric world!");
+	}
+}
---
+++ src/main/java/com/c0d3r_/thirdlife/ThirdLifeClient.java
@@ -0,0 +1,10 @@
+package com.c0d3r_.thirdlife;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ThirdLifeClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
